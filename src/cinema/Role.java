c9dbbb1944eaff25dde3cc9e914b74c3e90c0d97
federandink <federandink@gmail.com>
2017-03-29 15:57:53 +0200

    add role class halla wokbar

---
+++ src/cinema/Role.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package cinema;
+
+/**
+ * @author nihil
+ *
+ */
+public class Role {
+    private String nomRole;
+    private Film   film;
+    private Acteur acteur;
+    
+    
+    // -------------------Getters&Setters------------------------
+    /**
+     * @return the nomRole
+     */
+    public String getNomRole() {
+	return nomRole;
+    }
+    
+    
+    /**
+     * @return the film
+     */
+    public Film getFilm() {
+	return film;
+    }
+    
+    
+    /**
+     * @return the acteur
+     */
+    public Acteur getActeur() {
+	return acteur;
+    }
+    
+    
+    /**
+     * @param nomRole
+     * the nomRole to set
+     */
+    private void setNomRole(String nomRole) {
+	this.nomRole = nomRole;
+    }
+    
+    
+    /**
+     * @param film
+     * the film to set
+     */
+    private void setFilm(Film film) {
+	this.film = film;
+    }
+    
+    
+    /**
+     * @param acteur
+     * the acteur to set
+     */
+    private void setActeur(Acteur acteur) {
+	this.acteur = acteur;
+    }
+    
+}

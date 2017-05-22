4cee32b1712f8fb085952fc700339b0ca3c8d6bd
FederAndInk <federandink@gmail.com>
2017-05-22 12:22:23 +0200

    test format

--- src/cinema/Acteur.java
+++ src/cinema/Acteur.java
@@ -2,33 +2,40 @@ package cinema;
 
 import java.util.HashMap;
 
+
+
 /**
  *
  * @author culeta
  */
 public class Acteur implements java.io.Serializable {
-
-    private String nomActeur;
-    private HashMap<String, Role> roles = new HashMap<>(); //String = nom de film
-
+    
+    private String                nomActeur;
+    private HashMap<String, Role> roles = new HashMap<>(); // String = nom de film
+    
+    
     public Acteur(String nomActeur) {
-        this.setNomActeur(nomActeur);
+        setNomActeur(nomActeur);
     }
-
+    
+    
     private void setNomActeur(String nomActeur) {
         this.nomActeur = nomActeur;
     }
-
+    
+    
     public String getNomActeur() {
         return nomActeur;
     }
-
+    
+    
     public void addrole(Role role) {
         roles.put(role.getFilm().getNomFilm(), role);
     }
-
+    
+    
     public HashMap<String, Role> getRoles() {
         return roles;
     }
-
+    
 }
